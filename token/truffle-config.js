ac1a6cd5763570dc7ad5f4ee8fabd717f829986c
supercoolx <dm.kondratov.git@gmail.com>
2022-06-10 03:03:45 -0700

    Change config

--- token/truffle-config.js
+++ token/truffle-config.js
@@ -63,7 +63,8 @@ module.exports = {
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
+      skipDryRun: true,   // Skip dry run before migrations? (default: false for public nets )
+      networkCheckTimeout: 120000
     },
     // Useful for private networks
     // private: {
